681a49da160f316ef085482cd9b435274a343ab2
fayweb <fy.webster@gmail.com>
2021-09-29 16:26:09 +0200

    First intro into code summary

---
+++ Code_collection_1.R
@@ -0,0 +1,4 @@
+#here I can find all the code I have used written by myself or taken from others 
+#in this way I can easily scroll and search through the code, and save time in
+#the future 
+#please bear with me as I learn 
